947e342059368f8fea2377ba0bfe65b7ab806be3
Patrick <patrick@gmail.at>
2024-10-24 23:02:16 +0200

    changes

---
+++ ParkourApp/Dto/User.swift
@@ -0,0 +1,11 @@
+class User {
+    let username: String = "Patrick"
+    let description: String = "Hallo mein name ist Patrick"
+    let spotsAmount: Int32 = 2
+    let follower: Int32 = 4
+    let follows: Int32 = 2
+    
+    init() {
+        
+    }
+}
--- ParkourApp/Extensions/ColorExtension.swift
+++ ParkourApp/Extensions/ColorExtension.swift
@@ -2,4 +2,6 @@ import SwiftUI
 
 extension Color {
     static let primary = Color("PrimaryColor")
+    static let cardColor = Color("CardColor")
+    static let backgroundColor = Color("BackgroundColor")
 }
